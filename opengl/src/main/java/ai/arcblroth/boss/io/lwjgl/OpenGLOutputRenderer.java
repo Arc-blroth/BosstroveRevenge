c4af0cf4c2a2ea16d8e91228a1828fb9cca1319b
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2020-04-03 18:11:10 -0700

    Legacy rendering code is so commit ef29eaf8
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- opengl/src/main/java/ai/arcblroth/boss/io/lwjgl/OpenGLOutputRenderer.java
+++ opengl/src/main/java/ai/arcblroth/boss/io/lwjgl/OpenGLOutputRenderer.java
@@ -1,37 +1,37 @@
 package ai.arcblroth.boss.io.lwjgl;
 
-import org.joml.Matrix4f;
-import org.joml.Vector4f;
-import org.lwjgl.system.MemoryStack;
 import ai.arcblroth.boss.BosstrovesRevenge;
 import ai.arcblroth.boss.Relauncher;
 import ai.arcblroth.boss.crash.CrashReportGenerator;
 import ai.arcblroth.boss.io.IOutputRenderer;
-import ai.arcblroth.boss.render.*;
+import ai.arcblroth.boss.render.Color;
+import ai.arcblroth.boss.render.PixelAndTextGrid;
 import ai.arcblroth.boss.resource.ExternalResource;
 import ai.arcblroth.boss.resource.InternalResource;
 import ai.arcblroth.boss.resource.Resource;
 import ai.arcblroth.boss.util.Pair;
 import ai.arcblroth.boss.util.StaticDefaults;
 import ai.arcblroth.boss.util.TextureUtils;
+import org.joml.Matrix4f;
+import org.joml.Vector4f;
+import org.lwjgl.system.MemoryStack;
 
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.GL_BLEND;
-import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.glBlendFunc;
-import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL20.glGetUniformLocation;
-import static org.lwjgl.opengl.GL20.glUniform1i;
-import static org.lwjgl.opengl.GL30.*;
-import static org.lwjgl.system.MemoryUtil.NULL;
-
-import static ai.arcblroth.boss.io.lwjgl.OpenGLUtils.*;
-
-import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static ai.arcblroth.boss.io.lwjgl.OpenGLUtils.rgbToVector;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
+import static org.lwjgl.opengl.GL20.glUniform1i;
+import static org.lwjgl.opengl.GL30.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL30.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL30.glClear;
+import static org.lwjgl.opengl.GL30.glClearColor;
+import static org.lwjgl.opengl.GL30.glViewport;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class OpenGLOutputRenderer implements IOutputRenderer {
 
@@ -45,7 +45,6 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 	private GlfwInputHandler inputHandler;
 	
 	private Logger logger;
-	private Logger debugLinelogger;
 	
 	private boolean showFPS = true;
 	private double fps = 1;
@@ -55,7 +54,6 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 	public OpenGLOutputRenderer() {
 		try {
 			logger = Logger.getLogger("OpenGLOutputRenderer");
-			debugLinelogger = Logger.getLogger("DebugLine");
 			window = new Window("Bosstrove's Revenge", 0, 0);
 			inputHandler = new GlfwInputHandler();
 			
@@ -81,7 +79,7 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 			if(Relauncher.IS_WINDOWS && !Relauncher.IS_CYGWIN && !Relauncher.IS_MINGW_XTERM) {
 				Resource consolas = new ExternalResource("C:\\Windows\\Fonts\\consola.ttf");
 				if(consolas.exists()) {
-					logger.log(Level.INFO, "Using Consolas® as font.");
+					logger.log(Level.INFO, "Using Consolas(R) as font.");
 					font = consolas;
 				}
 			}
@@ -108,9 +106,10 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 		
 		//if(pg != null) {
 			if(!(System.getProperty(Relauncher.FORCE_NORENDER) != null && System.getProperty(Relauncher.FORCE_NORENDER).equals("true"))) {
-				
-				Vector4f clearColor = OpenGLUtils.rgbToVector(BosstrovesRevenge.instance().getResetColor());
-				glClearColor(clearColor.x, clearColor.y, clearColor.z, 1.0F);
+
+				Color clearColor = BosstrovesRevenge.instance().getResetColor();
+				Vector4f clearColorVec = OpenGLUtils.rgbToVector(clearColor);
+				glClearColor(clearColorVec.x, clearColorVec.y, clearColorVec.z, 1.0F);
 				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		        glEnable(GL_BLEND);
 		        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -125,45 +124,16 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 				glUniform1i(glGetUniformLocation(shader.getHandle(), "texture1"), 0);
 				
 				try (MemoryStack stack = MemoryStack.stackPush()) {
-					// Figure out the aspect ratio so we can maximize the final output
-					IntBuffer widthBuf = stack.mallocInt(1);
-					IntBuffer heightBuf = stack.mallocInt(1);
-					glfwGetWindowSize(window.getHandle(), widthBuf, heightBuf);
-					int width = widthBuf.get();
-					int height = heightBuf.get();
+					int width = window.getWidth();
+					int height = window.getHeight();
 					
 					lastSize = new Pair<Integer, Integer>(
 							2 * (int)Math.round((double)width / (double)StaticDefaults.CHARACTER_WIDTH),
 							2 * (int)Math.round((double)height / ((double)StaticDefaults.CHARACTER_HEIGHT / 2D))
 					);
-					
-					float aspectRatio;
-					float pixelSize;
-					/*
-					           r a t i o * 2                        2 
-					    +- - - - - - - - - - - - -         +- - - - - - - - -
-					    |                                  |
-					    |                                r |
-					    |                                a |
-					  2 |                                t |
-					    |                                i |
-					    |                                o |
-					    |                                * |    
-					                                     2 | 
-					                                       |
-					                                       |
-					*/
-					if(width > height) {
-						aspectRatio = (float)width / (float)height;
-						pixelSize = 1F / (height / ((float)StaticDefaults.CHARACTER_HEIGHT / 2F));
-						shader.setMatrix4f("projection", new Matrix4f().ortho(-aspectRatio, aspectRatio, -1, 1, 0, 1));
-						
-					} else {
-						aspectRatio = (float)height / (float)width;
-						pixelSize = 1F / (width / (float)StaticDefaults.CHARACTER_WIDTH);
-						shader.setMatrix4f("projection", new Matrix4f().ortho(-1, 1, -aspectRatio, aspectRatio, 0, 1));
-					}
-					Matrix4f scaledModelMatrix = new Matrix4f().scale(pixelSize, pixelSize, 1F);
+
+					shader.setMatrix4f("projection", new Matrix4f().ortho(-width, width, -height, height, 0, 1));
+					Matrix4f scaledModelMatrix = new Matrix4f().scale(StaticDefaults.CHARACTER_WIDTH, StaticDefaults.CHARACTER_WIDTH, 1F);
 					
 					// Render each row like a printer would
 					// Text is always printed on top of background stuff.
@@ -174,7 +144,7 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 						for (int colNum = 0; colNum < pg.getWidth(); colNum++) {
 							// Draw ordinary pixels
 							// If pixel color == background color, don't draw it!
-							if(!row1.get(colNum).equals(BosstrovesRevenge.instance().getResetColor())) {
+							if(!row1.get(colNum).equals(clearColor)) {
 								drawPixel(
 									new Matrix4f(scaledModelMatrix).translate(
 										(-pg.getWidth()/2F + colNum),
@@ -184,7 +154,7 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 									row1.get(colNum)
 								);
 							}
-							if(!row2.get(colNum).equals(BosstrovesRevenge.instance().getResetColor())) {
+							if(!row2.get(colNum).equals(clearColor)) {
 								drawPixel(
 									new Matrix4f(scaledModelMatrix).translate(
 											(-pg.getWidth()/2F + colNum),
@@ -223,33 +193,24 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 					//FPS + memory
 					if(showFPS) {
 						
-						//"Undo" the above aspect ratio.
-						float topTranslation, leftTranslation;
-						if(width > height) {
-							topTranslation = aspectRatio;
-							leftTranslation = 1F;
-						} else {
-							topTranslation = 1F;
-							leftTranslation = aspectRatio;
-						}
-						
 						//For inverted-ness
 						//glBlendFuncSeparate(GL_ONE_MINUS_DST_COLOR, GL_ONE_MINUS_SRC_ALPHA, GL_ZERO, GL_ZERO);
-						
+						Color invertedClearColor = TextureUtils.invert(clearColor);
+
 						String fpsString = String.format("%.0f FPS", fps);
 						for(int fpsCharIndex = 0; fpsCharIndex < fpsString.length(); fpsCharIndex++) {
 							drawCharacter(
 								new Matrix4f(scaledModelMatrix).translate(
-										(-topTranslation / pixelSize + fpsCharIndex + 0.25F),
-										( leftTranslation / pixelSize - 1.75F),
+										(-pg.getWidth()/2F + fpsCharIndex + 0.25F),
+										( pg.getHeight()/2F - 1.75F),
 										0
 								).scale(1F, -1F, 1F),
 								new Matrix4f(scaledModelMatrix).translate(
-										(-topTranslation / pixelSize + fpsCharIndex + 0.75F),
-										( leftTranslation / pixelSize - 0.75F),
+										(-pg.getWidth()/2F + fpsCharIndex + 0.75F),
+										( pg.getHeight()/2F - 0.75F),
 										0
 								).scale(0.5F, 1F, 1F),
-								new Pair<Color, Color>(Color.WHITE, null),
+								new Pair<Color, Color>(invertedClearColor, null),
 								fpsString.charAt(fpsCharIndex)
 							);
 						}
@@ -261,16 +222,16 @@ public class OpenGLOutputRenderer implements IOutputRenderer {
 						for(int memCharIndex = 0; memCharIndex < memString.length(); memCharIndex++) {
 							drawCharacter(
 								new Matrix4f(scaledModelMatrix).translate(
-										(topTranslation / pixelSize - memCharIndex - 1.25F),
-										(leftTranslation / pixelSize - 1.75F),
+										(pg.getWidth()/2F - memCharIndex - 1.25F),
+										(pg.getHeight()/2F - 1.75F),
 										0
 								).scale(1F, -1F, 1F),
 								new Matrix4f(scaledModelMatrix).translate(
-										(topTranslation / pixelSize - memCharIndex - 0.75F),
-										(leftTranslation / pixelSize - 0.75F),
+										(pg.getWidth()/2F - memCharIndex - 0.75F),
+										(pg.getHeight()/2F - 0.75F),
 										0
 								).scale(0.5F, 1F, 1F),
-								new Pair<Color, Color>(Color.WHITE, null),
+								new Pair<Color, Color>(invertedClearColor, null),
 								memString.charAt(memString.length() - 1 - memCharIndex)
 							);
 						}
